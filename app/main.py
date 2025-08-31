7cfc3f9eae21851aa1c379227a07f3adc7a68591
mike b <mukeshbarnwal.iit@gmail.com>
2025-08-31 22:42:34 +0530

    updated llm code

--- app/main.py
+++ app/main.py
@@ -2,18 +2,45 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 import requests
-
+import uvicorn
+from openai import OpenAI
+import enum
+from enum import Enum
+import instructor
 app = FastAPI()
-
 class InputText(BaseModel):
     text: str
 
-HF_API_URL = "https://api-inference.huggingface.co/models/distilbert-base-uncased-finetuned-sst-2-english"
-HEADERS = {"Authorization": "Bearer hf_XZQZQZQZQZQZQZQZQZQZQZQZQZQZQZQZ"}
+class ComplaintCategory(str, Enum):
+    product = "Product"
+    finance = "Finance"
+    shipping = "Shipping"
+    customer_service = "CustomerService"
+
+class OutputText(BaseModel):
+    category: ComplaintCategory
+        
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+# read the environment variables
+model=os.getenv("MODEL")
+api_key=os.getenv("OPENAI_API_KEY")
+
+client=OpenAI(api_key=api_key)
+
+client=instructor.patch(client)
 
 @app.post("/predict")
 def predict(input: InputText):
     payload = {"inputs": input.text}
-    response = requests.post(HF_API_URL, headers=HEADERS, json=payload)
-    result = response.json()
-    return {"sentiment": result}
+    prompt=f"Classify the following text into different types of complaints like product, finance, shipping, customer service, etc. \n Text: {input.text}"
+    response = client.chat.completions.create(
+        model=model,messages=[{"role":"user","content":prompt}], 
+        response_model=OutputText)
+    
+    return response
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="localhost", port=8000)
